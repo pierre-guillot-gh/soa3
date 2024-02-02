d7e123d155a646fe68c5c219f16e643836532ef6
Junhao Chen <junhao@hotmail.com>
2024-02-02 11:13:55 +0100

    Format cleanup

--- BioscoopCasus/Extensions/Extensions.cs
+++ BioscoopCasus/Extensions/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace BioscoopCasus.Extensions {
+﻿namespace BioscoopCasus.Extensions {
     public static class Extensions {
         public static bool IsWeekend(this DateTime dateTime) {
             return dateTime.DayOfWeek == DayOfWeek.Friday || dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday;
