228d1b9c8397489a5a7fc8673ba7cd7c0de0dbea
ErenAygun6 <e.aygun2@student.avans.nl>
2024-02-04 18:28:29 +0100

    Fixed formatting

--- BioscoopCasus/Domain/Order.cs
+++ BioscoopCasus/Domain/Order.cs
@@ -2,16 +2,13 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace BioscoopCasus.Domain
-{
-    public class Order
-    {
+namespace BioscoopCasus.Domain {
+    public class Order {
         [JsonPropertyName("OrderNumber")] public int OrderNr { get; set; }
         [JsonPropertyName("IsStudentOrder")] public bool IsStudentOrder { get; set; }
         [JsonPropertyName("Tickets")] public List<MovieTicket> Tickets { get; set; }
 
-        public Order(int orderNr, bool isStudentOrder)
-        {
+        public Order(int orderNr, bool isStudentOrder) {
             OrderNr = orderNr;
             IsStudentOrder = isStudentOrder;
             Tickets = new List<MovieTicket>();
@@ -20,18 +17,15 @@ namespace BioscoopCasus.Domain
         static MovieScreening movieScreening = new MovieScreening(new Movie(""), DateTime.Today, 10);
         static MovieTicket movieTicket = new MovieTicket(movieScreening, 1, 2, false);
 
-        public int GetOrderNr()
-        {
+        public int GetOrderNr() {
             return OrderNr;
         }
 
-        public void AddSeatReservation(MovieTicket ticket)
-        {
+        public void AddSeatReservation(MovieTicket ticket) {
             Tickets.Add(ticket);
         }
 
-        public double CalculatePrice()
-        {
+        public double CalculatePrice() {
             double sum = 0;
             var tickets = Tickets;
 
@@ -41,20 +35,14 @@ namespace BioscoopCasus.Domain
 
             bool isGroupDiscount = (isWeekend && Tickets.Count >= 6);
 
-            foreach (MovieTicket movieTicket in filtered)
-            {
-                if (movieTicket.IsPremium && IsStudentOrder)
-                {
+            foreach (MovieTicket movieTicket in filtered) {
+                if (movieTicket.IsPremium && IsStudentOrder) {
                     if (isGroupDiscount) sum += (movieTicket.MovieScreening.PricePerSeat + 3) * 0.9;
                     else sum += movieTicket.MovieScreening.PricePerSeat + 3;
-                }
-                else if (movieTicket.IsPremium && !IsStudentOrder)
-                {
+                } else if (movieTicket.IsPremium && !IsStudentOrder) {
                     if (isGroupDiscount) sum += (movieTicket.MovieScreening.PricePerSeat + 2) * 0.9;
                     else sum += movieTicket.MovieScreening.PricePerSeat + 2;
-                }
-                else
-                {
+                } else {
                     if (isGroupDiscount) sum += movieTicket.MovieScreening.PricePerSeat * 0.9;
                     else sum += movieTicket.MovieScreening.PricePerSeat;
                 }
@@ -62,22 +50,17 @@ namespace BioscoopCasus.Domain
             return sum;
         }
 
-        public void Export(TicketExportFormat exportFormat)
-        {
-            if (exportFormat == TicketExportFormat.PLAINTEXT)
-            {
+        public void Export(TicketExportFormat exportFormat) {
+            if (exportFormat == TicketExportFormat.PLAINTEXT) {
                 Console.WriteLine($"Order Number: {OrderNr}");
                 Console.WriteLine($"Is Student Order: {IsStudentOrder}");
                 Console.WriteLine("Tickets:");
-                foreach (var ticket in Tickets)
-                {
+                foreach (var ticket in Tickets) {
                     Console.WriteLine($"  {ticket}");
                 }
                 Console.WriteLine($"Total Price: {CalculatePrice()}");
                 File.WriteAllText("path.txt", this.ToString());
-            }
-            else if (exportFormat == TicketExportFormat.JSON)
-            {
+            } else if (exportFormat == TicketExportFormat.JSON) {
                 var json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
                 Console.WriteLine(json);
                 File.WriteAllText("path.json", json);
