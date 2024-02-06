9f2da1d9eb80cd52bbe5de35d8bd57cd0e33977a
Junhao Chen <junhao@hotmail.com>
2024-02-06 19:20:11 +0100

    Add tests

--- BioscoopCasus.Tests/OrderTests.cs
+++ BioscoopCasus.Tests/OrderTests.cs
@@ -1,8 +1,78 @@
+using BioscoopCasus.Domain;
+using System.Diagnostics;
 namespace BioscoopCasus.Tests {
     public class OrderTests {
         [Fact]
-        public void Test1() {
+        public void CalculatePrice_OneStandardTicket_Weekend_NonStudentOrder_ShouldCalculateCorrectPrice() {
+            // Arrange
+            Order order = CreateOrder(1, 10, false, true, false);
 
+            // Act
+            var totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(10, totalPrice);
+        }
+
+        [Fact]
+        public void CalculatePrice_FiveStandardTickets_Weekday_NonStudentOrder_ShouldCalculateCorrectPrice() {
+            // Arrange
+            Order order = CreateOrder(5, 10, false, false, false);
+
+            // Act
+            var totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(30, totalPrice);
+        }
+
+        [Fact]
+        public void CalculatePrice_EightStandardTickets_Weekend_NonStudentOrder_ShouldCalculateCorrectPrice() {
+            // Arrange
+            Order order = CreateOrder(8, 10, false, true, false);
+
+            // Act
+            var totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(36, totalPrice);
+        }
+
+        [Fact]
+        public void CalculatePrice_FiveStandardTicket_Weekday_StudentOrder_ShouldCalculateCorrectPrice() {
+            // Arrange
+            Order order = CreateOrder(5, 10, true, false, true);
+
+            // Act
+            var totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(36, totalPrice);
+        }
+
+        [Fact]
+        public void CalculatePrice_SixPremiumTicket_Weekday_NonStudentOrder_ShouldCalculateCorrectPrice() {
+            // Arrange
+            Order order = CreateOrder(6, 10, true, false, false);
+
+            // Act
+            var totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.Equal(39, totalPrice);
+        }
+
+        private Order CreateOrder(int numberOfTickets, decimal price, bool isPremium, bool isWeekend, bool isStudentOrder) {
+            Movie movie = new Movie("Spongebob");
+            DateTime date = isWeekend ? new DateTime(2024, 2, 10) : new DateTime(2024, 2, 6);
+            MovieScreening movieScreening = new MovieScreening(movie, date, price);
+            Order order = new Order(1, isStudentOrder);
+
+            for (int i = 0; i < numberOfTickets; i++) {
+                order.AddSeatReservation(new MovieTicket(movieScreening, 1, 1, isPremium));
+            }
+
+            return order;
         }
     }
 }
