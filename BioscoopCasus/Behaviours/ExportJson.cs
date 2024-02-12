f87c06be0f6356b672346ab1cf6098af47dddbec
Junhao Chen <junhao@hotmail.com>
2024-02-12 14:58:32 +0100

    Add classes

---
+++ BioscoopCasus/Behaviours/ExportJson.cs
@@ -0,0 +1,10 @@
+﻿using BioscoopCasus.Domain;
+using BioscoopCasus.Interfaces;
+
+namespace BioscoopCasus.Behaviours {
+    public class ExportJson : IExport {
+        public void Export(Order order) {
+            throw new NotImplementedException();
+        }
+    }
+}
